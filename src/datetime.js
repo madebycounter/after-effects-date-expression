08d5cd7757aecf8c1328e5bc57539e02e6887142
William Gardner <william@madebycounter.com>
2024-04-09 19:59:32 -0700

    Overhaul

--- src/datetime.js
+++ src/datetime.js
@@ -1,60 +1,16 @@
-// data from expression controls
-var progress = effect("Progress")("Slider");
-var should_render = effect("Enabled")("Checkbox");
+const SECONDS_PER_MINUTE = 60;
+const SECONDS_PER_HOUR = SECONDS_PER_MINUTE * 60;
+const SECONDS_PER_DAY = 24 * SECONDS_PER_HOUR;
 
-var difference = end_date.getTime() - start_date.getTime();
-var step = (progress * difference) / 100;
+const DAYS_PER_YEAR = 365;
+const DAYS_PER_LEAP_YEAR = DAYS_PER_YEAR + 1;
 
-function pad(val, len) {
-    return new String(val).padStart(len, "0");
-}
+const AM_PM = ["AM", "PM"];
 
-function ampm(hour) {
-    return hour < 12 ? "AM" : "PM";
-}
+const EPOCH_MONTH = 1;
+const EPOCH_YEAR = 1970;
 
-function twelve(hour) {
-    return hour > 12 ? hour - 12 : hour;
-}
-
-function render(dt, txt) {
-    let year = dt.getFullYear();
-    let month = dt.getMonth();
-    let date = dt.getDate();
-    let day = dt.getDay();
-    let hour = dt.getHours();
-    let minute = dt.getMinutes();
-    let second = dt.getSeconds();
-
-    return new String(txt)
-        .replaceAll("%a", days_abbr[day])
-        .replaceAll("%A", days[date])
-        .replaceAll("%b", months_abbr[month])
-        .replaceAll("%B", months[month])
-        .replaceAll("%c", dt.toLocaleString())
-        .replaceAll("%d", pad(date, 2))
-        .replaceAll("%H", pad(hour, 2))
-        .replaceAll("%I", pad(twelve(hour), 2))
-        .replaceAll("%j", "not implemented")
-        .replaceAll("%m", pad(month, 2))
-        .replaceAll("%M", pad(minute, 2))
-        .replaceAll("%p", ampm(hour))
-        .replaceAll("%S", pad(second, 2))
-        .replaceAll("%U", "not implemented")
-        .replaceAll("%w", day)
-        .replaceAll("%W", "not implemented")
-        .replaceAll("%x", dt.toLocaleDateString())
-        .replaceAll("%X", dt.toLocaleTimeString())
-        .replaceAll("%y", new String(year).substring(2, 4))
-        .replaceAll("%Y", year)
-        .replaceAll("%Z", "not implemented");
-}
-
-function conditional(bool, val1, val2) {
-    return bool ? val1 : val2;
-}
-
-var months = [
+const MONTH_NAMES = [
     "January",
     "February",
     "March",
@@ -68,44 +24,154 @@ var months = [
     "November",
     "December",
 ];
-var months_abbr = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-];
 
-var days = [
+const DAY_NAMES = [
+    "Sunday",
     "Monday",
     "Tuesday",
     "Wednesday",
     "Thursday",
     "Friday",
     "Saturday",
-    "Sunday",
 ];
 
-var days_abbr = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"];
-
-if (uppercase) {
-    months = months.map((m) => m.toUpperCase());
-    months_abbr = months_abbr.map((m) => m.toUpperCase());
-    days = days.map((m) => m.toUpperCase());
-    days_abbr = days_abbr.map((m) => m.toUpperCase());
+function getDayName(day) {
+    return uppercase ? DAY_NAMES[day].toUpperCase() : DAY_NAMES[day];
 }
 
-var ms = start_date.getTime() + step;
-var d = new Date(0);
-d.setTime(ms);
+function getMonthName(month) {
+    return uppercase ? MONTH_NAMES[month].toUpperCase() : MONTH_NAMES[month];
+}
+
+function isLeapYear(year) {
+    return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+}
+
+function mod(n, d) {
+    return ((n % d) + d) % d;
+}
+
+function dayOfWeek(ts) {
+    return mod(Math.floor(ts / 86400) + 4, 7);
+}
+
+function daysInYear(year) {
+    return isLeapYear(year) ? DAYS_PER_LEAP_YEAR : DAYS_PER_YEAR;
+}
+
+function daysInMonth(year) {
+    return [
+        31,
+        isLeapYear(year) ? 29 : 28,
+        31,
+        30,
+        31,
+        30,
+        31,
+        31,
+        30,
+        31,
+        30,
+        31,
+    ];
+}
+
+function getDateTime(ts, tz) {
+    ts = ts + tz * SECONDS_PER_HOUR;
+
+    days = Math.floor(ts / SECONDS_PER_DAY);
+    year = EPOCH_YEAR;
+
+    while (days >= daysInYear(year)) {
+        days -= daysInYear(year);
+        year++;
+    }
+
+    daysPerMonth = daysInMonth(year);
+    month = EPOCH_MONTH;
+
+    while (days >= daysPerMonth[month - 1]) {
+        days -= daysPerMonth[month - 1];
+        month++;
+    }
+
+    remaining = ts % SECONDS_PER_DAY;
+
+    date = days + 1;
+
+    day = dayOfWeek(ts);
+    hour = Math.floor(remaining / SECONDS_PER_HOUR);
+    minute = Math.floor(remaining / SECONDS_PER_MINUTE) % SECONDS_PER_MINUTE;
+    second = Math.floor(remaining % SECONDS_PER_MINUTE);
+
+    dayName = getDayName(day);
+    monthName = getMonthName(month - 1);
+
+    amPm = AM_PM[hour >= 12 ? 1 : 0];
+    hourTwelve = hour = ((hour + 11) % 12) + 1;
+
+    return {
+        year,
+        month,
+        date,
+        day,
+        hour,
+        minute,
+        second,
+        dayName,
+        monthName,
+        amPm,
+        hourTwelve,
+    };
+}
+
+function interpolateTime(start, end, percent) {
+    return start + (end - start) * percent;
+}
+
+function pad(val, len) {
+    return new String(val).padStart(len, "0");
+}
+
+function abbr(str) {
+    return str.substring(0, 3);
+}
+
+function render(ts, txt) {
+    let dt = getDateTime(ts, -7);
+
+    return new String(txt)
+        .replaceAll("%a", abbr(dt.dayName))
+        .replaceAll("%A", dt.dayName)
+        .replaceAll("%b", abbr(dt.monthName))
+        .replaceAll("%B", dt.month)
+        .replaceAll("%c", "NA")
+        .replaceAll("%d", pad(dt.date, 2))
+        .replaceAll("%H", pad(dt.hour, 2))
+        .replaceAll("%I", pad(dt.hourTwelve, 2))
+        .replaceAll("%j", "NA")
+        .replaceAll("%m", pad(dt.month, 2))
+        .replaceAll("%M", pad(dt.minute, 2))
+        .replaceAll("%p", dt.amPm)
+        .replaceAll("%S", pad(dt.second, 2))
+        .replaceAll("%U", "NA")
+        .replaceAll("%w", dt.day)
+        .replaceAll("%W", "NA")
+        .replaceAll("%x", "NA")
+        .replaceAll("%X", "NA")
+        .replaceAll("%y", new String(dt.year).substring(2, 4))
+        .replaceAll("%Y", dt.year)
+        .replaceAll("%Z", "NA");
+}
+
+var progress = effect("Progress")("Slider");
+var should_render = effect("Enabled")("Checkbox");
+
+var dateTime =
+    interpolateTime(start_date.getTime(), end_date.getTime(), progress / 100) /
+    1000;
 
 text.sourceText =
     should_render == 1
-        ? render(d, text.sourceText)
+        ? render(dateTime, text.sourceText)
         : new String(text.sourceText);
